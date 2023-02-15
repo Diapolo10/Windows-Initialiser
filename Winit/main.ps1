9127eb0ffa5a1ae6f028efb0c6236720968118f6
Lari Liuhamo <Diapolo10@users.noreply.github.com>
2023-02-16 00:35:25 +0200

    [PATCH] Final touches to CI/CD

--- Winit/main.ps1
+++ Winit/main.ps1
@@ -1,12 +1,21 @@
-$scriptpath = $MyInvocation.MyCommand.Path
-$parentdir = Split-Path $scriptpath -Parent
+
+# Resolves the problem of getting the parent directory in both script and executable form
+if ($MyInvocation.MyCommand.CommandType -eq "ExternalScript") {
+    $ParentDir = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+} else {
+    $ParentDir = Split-Path -Parent -Path ([Environment]::GetCommandLineArgs()[0]) 
+    if (!$ParentDir){
+        $ParentDir = "."
+    }
+}
+
 $VERSION = "0.1.0"
 
 Write-Output "Winit v$VERSION"
 
 Write-Output $(winget -v)
 
-$xml = [xml](Get-Content "$parentdir\applications.xml")
+$xml = [xml](Get-Content "$ParentDir\applications.xml")
 
 foreach($group in $xml.ProgramGroups.ChildNodes) {
     Write-Output $group
