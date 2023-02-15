c3aa3c8555baeeb95a0efa68935f5f4a7ca5b883
Lari Liuhamo <Diapolo10@users.noreply.github.com>
2023-02-15 23:56:19 +0200

    [PATCH] Testing indirect dependency inclusion

--- Winit/main.ps1
+++ Winit/main.ps1
@@ -1,20 +1,20 @@
-# $scriptpath = $MyInvocation.MyCommand.Path
-# $parentdir = Split-Path $scriptpath -Parent
+$scriptpath = $MyInvocation.MyCommand.Path
+$parentdir = Split-Path $scriptpath -Parent
 $VERSION = "0.1.0"
 
 Write-Output "Winit v$VERSION"
 
 Write-Output $(winget -v)
 
-# $xml = [xml](Get-Content "$parentdir\applications.xml")
+$xml = [xml](Get-Content "$parentdir\applications.xml")
 
-# foreach($group in $xml.ProgramGroups.ChildNodes) {
-#     Write-Output $group
-# }
+foreach($group in $xml.ProgramGroups.ChildNodes) {
+    Write-Output $group
+}
 
-# Write-Output ""
+Write-Output ""
 
-# foreach($program in ($xml.ProgramGroups.ProgramGroup|Where-Object {$_.Name -eq "Streaming"}).ChildNodes) {
-#     Write-Output $program.Id
+foreach($program in ($xml.ProgramGroups.ProgramGroup|Where-Object {$_.Name -eq "Streaming"}).ChildNodes) {
+    Write-Output $program.Id
     <# $(winget install $program.Id $program.InstallArgs) #>
-# }
+}
